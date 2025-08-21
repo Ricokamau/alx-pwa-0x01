6b5dd8b2f53d67fb58548f6b1125a66d7b09c290
Ricokamau <ricokamau@gmail.com>
2025-08-21 14:48:34 +0300

    link manifest and theme-color in _document

--- alx-movie-app/pages/_document.tsx
+++ alx-movie-app/pages/_document.tsx
@@ -3,7 +3,15 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#0070f3" />
+        {/* (Optional) Better PWA experience on Apple devices:
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <link rel="apple-touch-icon" href="/icons/apple-icon-152x152.png" />
+        */}
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
